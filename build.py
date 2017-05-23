e5c0a4f1c8cf3e5ae3a30146c7dfbe30eee189cd
sangam <sangam.angre@gmail.com>
2017-05-23 11:57:23 +0530

    test cases created

--- build.py
+++
@@ -1,8 +0,0 @@
-def sub_two(a, b):
-    if a is None or b is None:
-        raise TypeError('a or b cannot be None')
-    result = a ^ b;
-    borrow = (~a & b) << 1
-    if borrow != 0:
-        return sub_two(result, borrow)
-    return result
